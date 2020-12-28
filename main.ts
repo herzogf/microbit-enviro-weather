f172ed1cc032c49799360965b8a2910125713148
Florian <herzog-florian@gmx.de>
2020-12-28 20:48:33 +0100

    Show temperature and humidity on double clap

--- main.ts
+++ main.ts
@@ -4,12 +4,23 @@ input.onButtonPressed(Button.A, function () {
     basic.pause(500)
     basic.showIcon(IconNames.Heart)
 })
+envirobit.onClap(function () {
+    if (envirobit.timeSinceLastClap() < 500) {
+        basic.clearScreen()
+        basic.showString("" + envirobit.getTemperature() + " C")
+        basic.pause(500)
+        basic.showString("" + envirobit.getHumidity() + " %")
+        basic.pause(500)
+        basic.showIcon(IconNames.Heart)
+    }
+})
 input.onButtonPressed(Button.B, function () {
     basic.clearScreen()
     basic.showString("" + envirobit.getHumidity() + " %")
     basic.pause(500)
     basic.showIcon(IconNames.Heart)
 })
+envirobit.setClapSensitivity(80)
 envirobit.setLEDs(envirobit.OnOff.Off)
 basic.showIcon(IconNames.Heart)
 basic.forever(function () {
